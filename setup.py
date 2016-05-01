520e0cfc34132e20c0b7e69e36d830931e531bd0
Will <whp3652@rit.edu>
2016-05-01 01:35:29 -0400

    Clean project for distribution

--- setup.py
+++ setup.py
@@ -2,8 +2,9 @@ from setuptools import setup
 
 setup(
     name='py-lex',
-    version='0.0.1',
+    version='0.1.0',
     author='Will Paul',
     author_email='whp3652@rit.edu',
+    install_requires=['marisa_trie>=0.7', 'bidict>=0.11'],
     packages=['py-lex'],
 )
